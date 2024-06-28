3ff41829aa65e2a23879883245ac39a242ceb9b7
Sam McLeod <sammcj@users.noreply.github.com>
2024-06-28 16:00:58 +1000

    minor updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ echo "Starting TabbyLoader WebUI with endpoint URL: ${ENDPOINT_URL}"
 # trap SIGTERM and SIGINT to kill the python process in the container
 trap 'kill -TERM $PID' TERM INT
 
-if [[ -n "${AUTH}" ]]; then
+if [[ -n ${AUTH} ]] && [[ "${AUTH}" != "" ]]; then
   python3 /app/webui.py --listen --endpoint_url "${ENDPOINT_URL}" "${AUTH}"
 else
   python3 /app/webui.py --listen --endpoint_url "${ENDPOINT_URL}"
